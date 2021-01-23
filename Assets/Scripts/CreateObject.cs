1be3ddbb509d2b49d31ca31c10e0c7cfd8d3d07f
nerikara <47436064+nerikara@users.noreply.github.com>
2021-01-23 17:22:49 +0900

    レイキャスト

---
+++ Assets/Scripts/CreateObject.cs
@@ -0,0 +1,34 @@
+﻿// https://note.com/npaka/n/nc24ba42aa710
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class CreateObject : MonoBehaviour
+{
+    public GameObject ObjectPrefab;
+
+    private ARRaycastManager RaycastManager;
+    private List<ARRaycastHit> HitResults = new List<ARRaycastHit>();
+
+    // 初期化時に呼ばれる
+    void Awake()
+    {
+        RaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // フレーム毎に呼ばれる
+    void Update() {
+        // タッチ時
+        if (Input.GetMouseButtonDown(0))
+        {
+            // レイと平面が交差時
+            if (RaycastManager.Raycast(Input.GetTouch(0).position, HitResults, TrackableType.PlaneWithinPolygon))
+            {
+                // 3Dオブジェクトの生成
+                Instantiate(ObjectPrefab, HitResults[0].pose.position, Quaternion.identity);
+            }
+        }
+    }
+}
